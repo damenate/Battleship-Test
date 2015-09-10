0e006a2ff5ffa5c31ef6a2750332d12a33b65d14
Da-Me <damekim1206@gmail.com>
2015-09-10 18:34:26 -0400

    expanded on Human Player code to pass test 30

--- player.rb
+++ player.rb
@@ -1,3 +1,7 @@
+def get_user_input
+  gets.chomp
+end
+
 class Player
   attr_reader :name, :grid
 
@@ -19,9 +23,26 @@ class ComputerPlayer < Player
 end
 
 class HumanPlayer < Player
-  attr_reader :name
+  attr_reader :name, :grid, :ships
   def initialize (name = "Dave")
     @name = name
     @grid = Grid.new
+    @ships = []
+  end
+
+  def place_ships(ship_length = [2, 5])
+    counter = 0
+    loop do
+    puts "#{@name}, where would you like to place a ship of length #{}?"
+    input = get_user_input
+    x = @grid.x_of(input)
+    y = @grid.y_of(input)
+    puts "Across or Down?"
+    input = get_user_input
+    across == false if input == "Down"
+    @grid.place_ship(Ship.new(2), x, y, across)
+    @ships << Ship.new(2)
+    break if @ships.count == ship_length.count
+    end
   end
 end
