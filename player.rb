7df88ec5c8ef1fad567e51540c63700be5bfb760
Da-Me <damekim1206@gmail.com>
2015-09-10 20:40:04 -0400

    passes test 30

--- player.rb
+++ player.rb
@@ -24,6 +24,7 @@ end
 
 class HumanPlayer < Player
   attr_reader :name, :grid, :ships
+
   def initialize (name = "Dave")
     @name = name
     @grid = Grid.new
@@ -32,17 +33,27 @@ class HumanPlayer < Player
 
   def place_ships(ship_length = [2, 5])
     counter = 0
-    loop do
-    puts "#{@name}, where would you like to place a ship of length #{}?"
-    input = get_user_input
-    x = @grid.x_of(input)
-    y = @grid.y_of(input)
-    puts "Across or Down?"
-    input = get_user_input
-    across == false if input == "Down"
-    @grid.place_ship(Ship.new(2), x, y, across)
-    @ships << Ship.new(2)
-    break if @ships.count == ship_length.count
-    end
+    loop do |s|
+      s = ship_length[counter]
+      ship = Ship.new(s)
+      puts "#{@name}, where would you like to place a ship of length #{s}?"
+      input = get_user_input
+      x = @grid.x_of(input)
+      y = @grid.y_of(input)
+      puts "Across or Down?"
+      input = get_user_input
+      if input == "Across"
+        across = true
+      elsif input == "Down"
+        across = false
+      end
+       if @grid.place_ship(ship, x, y, across)
+         counter = counter + 1
+         @ships << ship
+       end
+         if @ships.count == ship_length.count
+           break
+         end
+     end
   end
 end
